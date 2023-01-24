83dee64564ee69cca25ad66c4c4a5a820eb9d7ae
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-24 15:34:44 -0300

    style: :lipstick: add article style on _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 
 //* styles *//
 import "../styles/globals.css";
+import "../styles/article.css";
 
 //* providers *//
 import { UIProvider } from "../context";
