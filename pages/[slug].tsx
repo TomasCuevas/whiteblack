d1b363c0b31bb806210b92a5a7b7fbdb08322ec7
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-24 15:33:43 -0300

    feat: :sparkles: add article page

---
+++ pages/[slug].tsx
@@ -0,0 +1,62 @@
+import { GetStaticProps, GetStaticPaths, NextPage } from "next";
+import { MDXRemote } from "next-mdx-remote";
+
+//* helpers *//
+import { getFileBySlug, getAllArticlesFiles } from "../utils";
+
+//* components *//
+import { ArticleHeader } from "../components/articles";
+
+//* layout *//
+import { MainLayout } from "../components/layout";
+
+//* styles *//
+import "highlight.js/styles/atom-one-dark.css";
+
+//* interfaces *//
+import { IArticleMetadata } from "../interfaces/IArticleMetadata";
+
+interface Props {
+  source: any;
+  metadata: IArticleMetadata;
+}
+
+const ArticlePage: NextPage<Props> = ({ metadata, source }) => {
+  return (
+    <MainLayout description={metadata.description} title={metadata.title}>
+      <article id="article" className="mx-auto max-w-[800px]">
+        <ArticleHeader metadata={metadata} />
+        <div>
+          <MDXRemote {...source} />
+        </div>
+      </article>
+    </MainLayout>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  const posts = await getAllArticlesFiles();
+  const paths = posts.map((post) => ({
+    params: {
+      slug: post.replace(".mdx", ""),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { metadata, source } = await getFileBySlug((params as any).slug);
+
+  return {
+    props: {
+      source,
+      metadata,
+    },
+  };
+};
+
+export default ArticlePage;
