cd85506e360398965dab5af3ae77e647b348c832
Tomas Cuevas <memo_mister398@hotmail.com>
2023-01-30 15:02:12 -0300

    feat: :sparkles: add NavLink component

---
+++ components/header/NavLink.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+//* interface *//
+interface Props {
+  link: string;
+  text: string;
+}
+
+export const NavLink: React.FC<Props> = ({ link, text }) => {
+  const { pathname } = useRouter();
+
+  return (
+    <li
+      className={`group flex items-center border-b-2 duration-300 hover:border-white ${
+        pathname === link ? "border-white/20" : "border-dark"
+      }`}
+    >
+      <Link
+        href={link}
+        className="flex h-full items-center justify-center px-5"
+      >
+        <span
+          className={`font-sans text-lg font-light ${
+            pathname === link ? "text-white" : "text-white/50"
+          } duration-300 group-hover:text-white`}
+        >
+          {text}
+        </span>
+      </Link>
+    </li>
+  );
+};
