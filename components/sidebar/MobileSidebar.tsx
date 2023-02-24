70f42dfb1f38e7f03ee6095807320b1c90c7da3c
Tomas Cuevas <memo_mister398@hotmail.com>
2023-02-24 17:07:33 -0300

    style: :lipstick: update icons on MobileSidebar and SidebarLink components

--- components/sidebar/MobileSidebar.tsx
+++ components/sidebar/MobileSidebar.tsx
@@ -1,12 +1,13 @@
-import Link from "next/link";
 import { useContext } from "react";
 
 //* icons *//
-import { MdClose } from "react-icons/md";
+import { RiCloseFill } from "react-icons/ri";
+
+//* components *//
+import { SidebarLink } from "./SidebarLink";
 
 //* context *//
 import { UIContext } from "../../context";
-import { SidebarLink } from "./SidebarLink";
 
 export const MobileSidebar: React.FC = () => {
   const { toggleSidebar } = useContext(UIContext);
@@ -15,7 +16,7 @@ export const MobileSidebar: React.FC = () => {
     <aside className="fixed top-0 left-0 z-50 h-screen w-screen bg-dark/90 backdrop-blur-sm sm:hidden">
       <div className="flex h-16 w-full items-end justify-center px-4">
         <div className="flex h-14 w-full items-center">
-          <MdClose
+          <RiCloseFill
             onClick={toggleSidebar}
             className="ml-auto h-10 w-10 text-purple"
           />
--- components/sidebar/SidebarLink.tsx
+++ components/sidebar/SidebarLink.tsx
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 
 //* icons *//
-import { MdArrowRight } from "react-icons/md";
+import { RiArrowDropRightFill } from "react-icons/ri";
 
 //* context *//
 import { UIContext } from "../../context";
@@ -24,17 +24,17 @@ export const SidebarLink: React.FC<Props> = ({ link, text }) => {
         href={link}
         className={`flex w-full items-center gap-2 rounded-br-md ${
           pathname === link ? "border-white" : "border-white/70"
-        } border-b border-r-2 py-2 pr-5 duration-300`}
+        } border-b border-r-2 py-2 pr-5 font-roboto`}
       >
-        <MdArrowRight
+        <RiArrowDropRightFill
           className={`text-4xl ${
             pathname === link ? "text-purple" : "text-purple/70"
-          } duration-300 group-hover:text-purple`}
+          } group-hover:text-purple`}
         />
         <span
           className={`text-xl font-light ${
-            pathname === link ? "text-white" : "text-white/70"
-          } duration-300 group-hover:text-white`}
+            pathname === link ? "text-white" : "text-white/50"
+          } group-hover:text-white`}
         >
           {text}
         </span>
