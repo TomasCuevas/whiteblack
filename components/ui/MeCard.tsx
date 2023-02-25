f5ff7241fcef1ffcf7912a761d4dc6f8f13951c7
Tomas Cuevas <memo_mister398@hotmail.com>
2023-02-25 16:39:42 -0300

    feat: :sparkles: add MeCard component

---
+++ components/ui/MeCard.tsx
@@ -0,0 +1,25 @@
+export const MeCard = () => {
+  return (
+    <div className="mt-10 hidden lg:block">
+      <div className="flex max-w-xs flex-col gap-4 border-l border-light py-4 px-4">
+        <div className="h-[95px] w-[95px] overflow-hidden rounded-full">
+          <img
+            src="/images/me/4x4.jpg"
+            alt="me image"
+            className="rounded-full object-cover object-center"
+          />
+        </div>
+        <div className="flex flex-col gap-1">
+          <h2 className="whitespace-nowrap font-merriweather text-lg text-white">
+            Anselmo Tomás Cuevas
+          </h2>
+          <p className="font-roboto text-sm text-gray-400">
+            Programador FullStack, que le gusta enseñar y compartir lo que va
+            aprendiendo en este camino tan apasionante del desarrollo de
+            software.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
