4a3c0f7237ae1d2a81831e17fbf5fedb0d140fff
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-09 01:13:01 -0300

    feat: add copyright on Footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,11 +1,12 @@
 import Link from "next/link";
 
 //* icons *//
-import { FaLinkedinIn, FaTwitter, FaGithub } from "react-icons/fa";
+import { FaLinkedinIn, FaGithub } from "react-icons/fa";
 
 export const Footer: React.FC = () => {
   return (
     <footer className="mt-auto flex w-full justify-center bg-light/50 px-4 py-6 sm:px-6">
+      <p className="font-inter text-white">Copyright © 2023 Tomás Cuevas</p>
       <div className="flex w-full max-w-[1000px] justify-evenly gap-10 sm:justify-end">
         <Link
           href="https://github.com/TomasCuevas"
@@ -13,13 +14,6 @@ export const Footer: React.FC = () => {
           className="group flex w-5 items-center"
         >
           <FaGithub className="h-full w-full text-purple/60 duration-300 hover:text-purple" />
-        </Link>
-        <Link
-          href="https://twitter.com/TomasC0D1NG"
-          target="_blank"
-          className="flex w-5 items-center"
-        >
-          <FaTwitter className="h-full w-full text-purple/60 duration-300 hover:text-purple" />
         </Link>
         <Link
           href="https://www.linkedin.com/in/tom%C3%A1s-cuevas-dev/"
