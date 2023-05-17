9f90a2a50e2950cb9835dd17ae191b548c0095a4
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-17 02:44:41 -0300

    feat: :children_crossing: hide header when it's scrolling down

--- components/layout/header/header/Header.tsx
+++ components/layout/header/header/Header.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import Link from "next/link";
 
 //* icons *//
@@ -7,16 +7,49 @@ import { RiMenu3Line } from "react-icons/ri";
 //* components *//
 import { NavLink } from "@/components/layout";
 
+//* styles *//
+import Styles from "./header.module.css";
+
 //* context *//
 import { UIContext } from "@/context";
 
 export const Header: React.FC = () => {
   const { toggleSidebar } = useContext(UIContext);
 
+  const [isVisible, setIsVisible] = useState<boolean>(true);
+  const scrollPosRef = useRef<number>(0);
+  const timeoutRef = useRef<any>(null);
+
+  const handleScroll = () => {
+    const currentScrollPos = window.pageYOffset;
+
+    setIsVisible(
+      currentScrollPos === 0 ? true : currentScrollPos < scrollPosRef.current
+    );
+
+    if (currentScrollPos > scrollPosRef.current) {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = setTimeout(() => {
+        setIsVisible(true);
+      }, 1000);
+    }
+
+    scrollPosRef.current = currentScrollPos;
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <header
       id="header"
-      className="fixed top-0 left-0 z-30 flex h-16 w-screen items-end justify-center border-b border-purple/20 bg-light/20  backdrop-blur-md md:h-[70px]"
+      className={`fixed top-0 left-0 z-30 flex h-16 w-screen items-end justify-center border-b border-purple/20 bg-light/20  backdrop-blur-[6px] md:h-[70px] ${
+        isVisible ? Styles.visible : Styles.scrolled
+      }`}
     >
       <div className="flex h-full w-full max-w-[1200px] items-center px-4 sm:px-6 xl:px-0">
         <Link href="/">
