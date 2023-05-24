d093861c235b46afe4e5aea46424ccb0a849fc87
Tomas Cuevas <memo_mister398@hotmail.com>
2023-05-24 11:50:01 -0300

    chore: :label: update IArticleMetadata interface and add IReadingTime interface

--- interfaces/IArticleMetadata.ts
+++ interfaces/IArticleMetadata.ts
@@ -1,12 +1,21 @@
 export interface IArticleMetadata {
   author: string;
-  link: string;
+  cardDescription: string;
   category: string;
   date: string;
   description: string;
-  cardDescription: string;
+  image?: string;
   keywords: string;
+  link: string;
+  readingTime: IReadingTime;
   slug: string;
   tags: string[];
   title: string;
+}
+
+interface IReadingTime {
+  minutes: number;
+  text: string;
+  time: number;
+  words: number;
 }
