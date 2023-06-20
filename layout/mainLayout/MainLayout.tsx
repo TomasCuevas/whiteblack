94965de94b3513c426aeb8024d8a0a1d11ee60d3
Tomas Cuevas <memo_mister398@hotmail.com>
2023-06-20 14:31:45 -0300

    style: :lipstick: update style on Main Layout

--- layout/mainLayout/MainLayout.tsx
+++ layout/mainLayout/MainLayout.tsx
@@ -30,11 +30,12 @@ export const MainLayout: React.FC<Props> = ({
       <MobileSidebar />
 
       <div className="fixed top-0 left-0 -z-20 h-screen w-screen bg-dark" />
-      <main className="mx-auto mt-20 mb-8 w-full max-w-[1200px] px-4 pt-4 sm:px-6 xl:px-0">
-        {children}
+      <main className="fixed top-[57px] flex h-[calc(100vh_-_57px)] w-full flex-col overflow-y-auto">
+        <div className="mx-auto w-full max-w-[1200px] px-4 py-4 sm:px-6 xl:px-0">
+          {children}
+        </div>
+        <Footer />
       </main>
-
-      <Footer />
     </>
   );
 };
