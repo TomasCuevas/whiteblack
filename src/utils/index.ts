e71bafa3f152c2f040edc182fb75cda4faad74d5
Tomas Cuevas <memo_mister398@hotmail.com>
2023-07-20 20:57:38 -0300

    chore: :fire: remove handleScroll util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,4 @@
 export * from "./getAllSectionsToSidebar/getAllSectionsToSidebar";
-export * from "./handleScroll/handleScroll";
 export * from "./mdxArticlesTransform/mdxArticlesTransform";
 export * from "./mdxCategoriesTransform/mdxCategoriesTransform";
 export * from "./observers/observers";
