7b6d60bb8fc116336174362fd9d947e809fd8f37
Tomas Cuevas <memo_mister398@hotmail.com>
2023-07-25 15:59:54 -0300

    fix: :bug: add scrollTo when pagination buttons is clicked

--- src/components/ui/PaginationButtons/PaginationButtons.tsx
+++ src/components/ui/PaginationButtons/PaginationButtons.tsx
@@ -20,7 +20,13 @@ export const PaginationButtons: React.FC<Props> = ({
   return (
     <div className="flex gap-3 [&>button]:flex [&>button]:items-center [&>button>svg]:text-4xl [&>button>svg]:text-purple dark:[&>button>svg]:text-purple/70 [&>button>span]:font-merriweather [&>button>span]:text-sm [&>button>span]:text-purple dark:[&>button>span]:text-purple/70 [&>button>span]:xs:text-lg">
       {pagination > 1 ? (
-        <button onClick={decrement} className="group mr-auto">
+        <button
+          onClick={() => {
+            window.scrollTo(0, 0);
+            decrement();
+          }}
+          className="group mr-auto"
+        >
           <RiArrowDropLeftFill className="group-hover:text-purple" />
           <span className="group-hover:text-purple">
             artículos más recientes
@@ -28,7 +34,13 @@ export const PaginationButtons: React.FC<Props> = ({
         </button>
       ) : null}
       {pagination * itemsPerPage < itemsLength ? (
-        <button onClick={increment} className="group ml-auto">
+        <button
+          onClick={() => {
+            window.scrollTo(0, 0);
+            increment();
+          }}
+          className="group ml-auto"
+        >
           <span className="group-hover:text-purple">artículos anteriores</span>
           <RiArrowDropRightFill className="group-hover:text-purple" />
         </button>
