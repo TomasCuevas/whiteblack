66ec1615ce0403d399622660f017ea468f9833f4
Tomas Cuevas <memo_mister398@hotmail.com>
2023-07-31 12:37:39 -0300

    fix: :bug: fix a bug in application theme

--- src/components/ui/ThemeSwitch/ThemeSwitch.tsx
+++ src/components/ui/ThemeSwitch/ThemeSwitch.tsx
@@ -5,24 +5,35 @@ import { useTheme } from "next-themes";
 import { BsSun, BsMoon } from "react-icons/bs";
 
 export const ThemeSwitch: React.FC = () => {
-  const { theme, setTheme, systemTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
   const [currentTheme, setCurrentTheme] = useState<any>();
 
   useEffect(() => {
-    setCurrentTheme(() => (theme === "system" ? systemTheme : theme));
+    setTheme(theme === "light" ? "light" : "dark");
+  }, []);
+
+  useEffect(() => {
+    setCurrentTheme(() => (theme === "light" ? "light" : "dark"));
+  }, [theme]);
+
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--scrollbar-track-color",
+      theme === "light" ? "#04030c" : "#e2e8f0"
+    );
   }, [theme]);
 
   return (
     <button
       onClick={() =>
-        currentTheme === "dark" ? setTheme("light") : setTheme("dark")
+        currentTheme === "light" ? setTheme("dark") : setTheme("light")
       }
       className="py-2 px-[10px] group shadow-inner rounded-md shadow-black/20 dark:shadow-slate-400/20"
     >
-      {currentTheme == "dark" ? (
-        <BsSun className="text-lg text-slate-200 group-hover:text-white" />
-      ) : (
+      {currentTheme == "light" ? (
         <BsMoon className="text-lg text-slate-800 group-hover:text-dark" />
+        ) : (
+        <BsSun className="text-lg text-slate-200 group-hover:text-white" />
       )}
     </button>
   );
